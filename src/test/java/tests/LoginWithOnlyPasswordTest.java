2f096283a9cfacf01ea852a92786947574529549
bahojuk <bahojuk@gmail.com>
2026-03-22 23:56:01 +0300

    Добавил Login тесты

---
+++ src/test/java/tests/LoginWithOnlyPasswordTest.java
@@ -0,0 +1,31 @@
+
+package tests;
+
+import core.base.BaseTest;
+import core.base.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginWithOnlyPasswordTest extends BaseTest {
+    private static LoginPage loginPage;
+
+    @BeforeEach
+    public void prepare() {
+        open(baseUrl);
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void loginWithOnlyUsernameTest() {
+        loginPage.loginPassword("test");
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        String expectedErrorMessage = "Введите логин";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+}
